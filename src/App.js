f675297cbb2aa3c163276445ecf6fab8c19a50ca
Wujunxian <wu812730157@gmail.com>
2019-04-15 11:12:05 +0800

    多项修改
    - 自适应高度宽度调整
    - 打错字的选项，用户可以选择，不需要输入序号
    - 用户不选择时，会提示用户继续输入

--- src/App.js
+++ src/App.js
@@ -5,27 +5,57 @@ import ChatBot, {Loading} from 'react-simple-chatbot'
 /**
  * 解析中间结果字符串，让用户进一步搜索
  * @param {string} str 需要解析的字符串
- * @returns {string[] | undefined} 若结果空，则返回undefined
+ * @returns {{message: string, choices: {label: string, value: string}[]} | undefined} 若结果空，则返回undefined
  */
 function parseUnsureResult(str) {
   let pat = /（([^.）]+)(?:...)?）$/;
   let parseResult = pat.exec(str);
   if (parseResult === null)
     return undefined;
-  return parseResult[1].split("、");
+  let labels = parseResult[1].split("、");
+  let choices = [];
+  for (let i = 0; i < labels.length; i++) {
+    choices.push({label: labels[i], value: labels[i]});
+  }
+  return {message: str.substr(0, str.indexOf('（')), choices: choices};
+}
+
+/**
+ * 解析返回的拼音错误、字错误选项，给用户选择
+ * @param {string} str
+ * @return {{message: string, choices: {label:string, value:string}[]}}
+ */
+function parseCorrectionResult(str) {
+  if (str.indexOf('输入选项：') !== -1) {
+    let message = str.substr(0, str.indexOf('('));
+    let choices = [];
+    let splits = str.split('\n');
+    for (let i = 1; i < splits.length - 1; i++) {
+      choices.push({value: splits[i].substr(0, splits[i].indexOf(':')),
+        label: splits[i]});
+    }
+    return {message: message, choices: choices}
+  }
+  return undefined;
 }
 
 /**
  * 解析最终结果字符串，并返回
  * @param {string} str
+ * @return {{message: string, choices: {label:string, value:string}[]}}
  */
 function parseResult(str) {
-  if (str.indexOf('\n') !== -1) {
-    // 若能够找到换行符，代表有结果传回来
-    let results = str.split("\n");
+  if (str.indexOf('歌') !== -1 && str.indexOf('\n') !== -1) {
+    // 若能够找到换行符和`歌`字，代表有结果传回来
+    let labels = str.split("\n");
+    let message = labels[0].substr(0, labels[0].indexOf('：'));
     // 当前第0个和最后一个结果是没有用的
-    results = results.slice(1, results.length - 1);
-    return results;
+    labels = labels.slice(1, labels.length - 1);
+    let results = [];
+    for (let i = 0; i < labels.length; i++) {
+      results.push({label: labels[i], value: labels[i]});
+    }
+    return {choices: results, message: message};
   }
   return undefined;
 }
@@ -47,17 +77,16 @@ class Query extends Component {
       if (this.readyState === 4) {
         const data = JSON.parse(this.responseText);
         const textResult = data.response;
-        let results;
+        let result;
         let nextStep = {trigger: 'user-input'};
         let nextMessage = textResult;
-        if ((results = parseUnsureResult(textResult)) !== undefined) {
-          nextStep.value = results;
+        // 解析字符串内容，并对有选项的内容提取出选项
+        if ((result = parseUnsureResult(textResult)) !== undefined
+          || (result = parseResult(textResult)) !== undefined
+          || (result = parseCorrectionResult(textResult)) !== undefined) {
+          nextStep.value = result.choices;
           nextStep.trigger = 'choices';
-          nextMessage = textResult.substr(0, textResult.indexOf('（'));
-        } else if ((results = parseResult(textResult)) !== undefined) {
-          nextStep.value = results;
-          nextStep.trigger = 'choices';
-          nextMessage = textResult.substr(0, textResult.indexOf('：'));
+          nextMessage = result.message;
         }
 
         self.setState({result: <div>{nextMessage}</div>}, () => {
@@ -77,6 +106,7 @@ class Query extends Component {
 
 /**
  * 将选项变成列表，单击可继续搜索
+ * 选项是一个对象：{label: string, value: string}
  */
 class Choices extends Component {
   constructor(props) {
@@ -92,9 +122,9 @@ class Choices extends Component {
     for (let i = 0; i < choices.length; i++) {
       rows.push(
         <div key={i} className={this.state.disabled ? "list-item list-item-disabled" : "list-item"} onClick={() => {
-          self.props.triggerNextStep({value: choices[i], trigger: 'user-selected'});
+          self.props.triggerNextStep({value: choices[i].value, trigger: 'user-selected'});
           self.setState({disabled: true});
-        }}>{choices[i]}</div>
+        }}>{choices[i].label}</div>
       )
     }
 
@@ -102,8 +132,8 @@ class Choices extends Component {
     rows.push(
       <div key={'b'} className={this.state.disabled ? "list-item list-item-disabled" : "list-item"} onClick={() => {
         self.setState({disabled: true});
-        self.props.triggerNextStep({trigger: 'user-input'});
-      }}>不选择</div>
+        self.props.triggerNextStep({trigger: 'not-my-choice'});
+      }}>没有我想要的</div>
     );
 
     return <div style={{width: "100%", pointerEvents: this.state.disabled ? "none" : "default"}}>
@@ -129,7 +159,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ChatBot botDelay={0} userDelay={0}
+        <ChatBot botDelay={0} userDelay={0} style={{width: "100vw", height: "100vh"}}
+                 contentStyle={{height: "calc(100vh - 112px)"}}
                  steps={[
                    {
                      id: '1',
@@ -158,6 +189,11 @@ class App extends Component {
                      component: <Choices/>,
                      waitAction: true,
                      delay: 0,
+                   },
+                   {
+                     id: 'not-my-choice',
+                     message: '那您想找什么？',
+                     trigger: 'user-input'
                    }
                  ]}
         />
